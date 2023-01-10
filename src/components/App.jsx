3ce9c45babfb4a45720a60b506ea3df0e1cb9f10
MishynaIra <gortenzia1987@gmail.com>
2023-01-10 11:11:10 +0200

    отправила запрос на сервер и получила ответ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import axios, { get } from 'axios';
+
+
+export class App extends Component {
+  state: {
+    images: null,
+  }
+
+  async componentDidMount() {
+    try {
+      const data =  axios.get('https://pixabay.com/api/?q=cat&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12');
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
 };
