57d6556a52f65bc3a7610378949f298e3e8e8cca
MishynaIra <gortenzia1987@gmail.com>
2023-01-11 15:54:45 +0200

    поисковый запрос передается в компонент АПИ. componentDidUpdate в процессе реализации

--- src/components/PixibayApi.jsx
+++ src/components/PixibayApi.jsx
@@ -21,46 +21,39 @@ export class PixabayApi extends Component {
     // status: 'idle',
   };
 
-  async componentDidMount() {
-    console.log('propsPixy', this.props.searchData);
-    const  response = await axios.get(`https://pixabay.com/api/?q=${this.props.searchData}&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12`)
-    console.log('response', response.data.hits);
+  // async componentDidMount() {
+  //   console.log('propsPixy', this.props.searchData);
+  //   const  response = await axios.get(`https://pixabay.com/api/?q=${this.props.searchData}&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12`)
+  //   console.log('response', response.data.hits);
+  //
+  // }
 
+  async componentDidUpdate(prevProps, prevState, snapshot) {
+
+    if (prevProps.searchData !== this.props.searchData) {
+
+      fetch( `https://pixabay.com/api/?q=${this.props.searchData}&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error('нет картинки')
+          );
+        })
+        .then(images => {
+          this.setState({ images })
+        })
+
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({loading: false}));
+    }
   }
 
   render() {
-    console.log('propsPixyRender', this.props.searchData);
-    return <div>j</div>
+    console.log('this.state.images',this.state.images);
+    return <div>хуй</div>
   }
 }
-
-  // async componentDidUpdate(prevProps, prevState, snapshot) {
-
-    // if (prevProps.image !== this.props.image) {
-    //    // console.log('изминили картинку');
-    //
-    //
-    //   fetch( 'https://pixabay.com/api/?q=cat&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12')
-    //     .then(response => {
-    //       if (response.ok) {
-    //         return response.json();
-    //       }
-    //       return Promise.reject(new Error('нет картинки')
-    //       );
-    //     })
-    //     .then(picture => {
-    //       this.setState({ picture })
-    //       console.log('this.state.picture',this.state.picture.hits[0].id)
-    //     })
-    //
-    //     .catch(error => this.setState({ error }))
-    //     .finally(() => this.setState({loading: false}));
-      // console.log('изминили картинку');
-      // console.log('prevProps', prevProps.image);
-      // console.log('this.props.image', this.props.image);
-  //   }
-  // }
-
 
 
 //   render() {
@@ -80,17 +73,3 @@ export class PixabayApi extends Component {
 //     );
 //   }
 // }
-
-
-
-// async componentDidMount() {
-//   this.setState({loading: true});
-//   try {
-//     const data =  axios.get(
-//       'https://pixabay.com/api/?q=cat&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12');
-//     this.setState({images: data});
-//   } catch (error) {
-//     console.log(error);
-//   }
-//   this.setState({loading: false});
-// };
