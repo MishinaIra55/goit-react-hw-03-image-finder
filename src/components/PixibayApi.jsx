77693fbf28d725c60020637d7b42bc4ff28dca19
MishynaIra <gortenzia1987@gmail.com>
2023-01-12 10:33:36 +0200

    state car(не отрабатывает ошибку)

--- src/components/PixibayApi.jsx
+++ src/components/PixibayApi.jsx
@@ -14,11 +14,11 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 // );
 
 export class PixabayApi extends Component {
-  state ={
-      images: [],
-      isLoading: false,
-    // error: null,
-    // status: 'idle',
+  state = {
+    images: [],
+
+    error: null,
+    status: 'idle',
   };
 
 
@@ -26,55 +26,69 @@ export class PixabayApi extends Component {
 
     if (prevProps.searchData !== this.props.searchData) {
 
-      fetch( `https://pixabay.com/api/?q=${this.props.searchData}&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12`)
+      this.setState({ status: 'pending' });
+
+      fetch(`https://pixabay.com/api/?q=${this.props.searchData}&page=1&key=31487728-a23d010ad4dc914c660c439a4&image_type=photo&orientation=horizontal&per_page=12`)
         .then(response => {
           if (response.ok) {
             return response.json();
           }
-          return Promise.reject(new Error('нет картинки')
+          return Promise.reject(
+            new Error(`нет картинки ${this.props.searchData}`),
           );
         })
         .then(images => {
-          this.setState({ images: images.hits })
+          this.setState({ images: images.hits, status: 'resolved'})
 
         })
 
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({loading: false}));
+        .catch(error => this.setState({ error, status: 'rejected' }))
+
+
+
     }
   }
 
   render() {
-    console.log('state.length', this.state.images.length);
-    console.log('state', this.state.images);
-    return (
 
-      <div>
-        {this.state.error && <div>{this.state.error.message}</div>}
-        {this.state.loading && <div>Loading....</div>}
-        {!this.props.searchData && <div>Введите ваш запрос поиска</div>}
-        {this.state.images.length > 0 && <div>{this.state.images[0].id}</div>}
-        {/*{this.state.images.length > 0 && <ImageGalley images={this.state.images}/>}*/}
-      </div>
-    )
+    const { images, error, status } = this.state;
+
+    // console.log('state.length', this.state.images.length);
+    // console.log('state', this.state.images);
+
+    if (status === 'idle') {
+      return <div>Введите ваш запрос поиска</div>
+    }
+
+    if (status === 'pending') {
+      return <div>Loading....</div>
+    }
+
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>
+    }
+
+    if (status === 'resolved') {
+      return <p>{images[0].id}</p>
+    }
   }
 }
+    // return (
+    //
+    //   <div>
+    //     {error && <h1>{error.message}</h1>}
+    //     {loading && <div>Loading....</div>}
+    //     {!searchData && <div>Введите ваш запрос поиска</div>}
+    //     {images.length > 0 &&
+    //       <div>
+    //         <p>{images[0].id}</p>
+    //
+    //         </div>}
+    //     {/*{this.state.images.length > 0 && <ImageGalley images={this.state.images}/>}*/}
+    //   </div>
+    // )
 
 
-//   render() {
-//
-//     return (
-//       <div>
-//         {/*{this.state.error && <div>{this.state.error.message}</div>}*/}
-//         {/*{this.state.loading && <div>Loading....</div>}*/}
-//         {/*{!this.props.image && <div>Введите ваш запрос поиска</div>}*/}
-//         {/*{this.state.picture &&*/}
-//         {/*  <div>*/}
-//         {/*  <img src={this.state.picture } alt="" />*/}
-//
-//         {/*  </div>}*/}
-//
-//     </div>
-//     );
-//   }
-// }
+
+
+
